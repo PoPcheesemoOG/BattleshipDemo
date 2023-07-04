fbd0f229f270e6c2750420dee5c52b9c1f322b29
Paul Helske <116106386+PoPcheesemoOG@users.noreply.github.com>
2023-07-03 20:01:06 -0600

    Update BoardSquare.java

--- BoardSquare.java
+++ BoardSquare.java
@@ -1,19 +1,23 @@
 
 public class BoardSquare {
-	public enum SquareState {Water, HiddenBoatPiece, ExposedBoatPiece};
-	public static SquareState state;
-	
-	public int boatRow = Battleship.getRowSize();
-	public int boatCol = Battleship.getColSize();
 
-	public static void hideBoats(int boatRow, int boatCol) {
-		for (int i = 0; i < boatRow; i++) {
-			state = SquareState.HiddenBoatPiece;
-		}
-		for (int i = 0; i < boatCol; i++) {
-			state = SquareState.HiddenBoatPiece;
-		}
-		
-		
+	public enum SquareState {Water, Hidden_Boat_Piece, Exposed_Boat_Piece};
+	public SquareState state = SquareState.Water;
+
+	static int height = GameBoard.height;
+	static int width = GameBoard.width;
+	
+	public int boatRow;
+	public int boatCol;
+	
+	public BoardSquare(SquareState newstate, int boatRow, int boatCol) {
+		state = newstate;
+		// boatRow = Battleship.rowsize;
+		// boatCol = Battleship.colsize;
+	}
+	
+	public SquareState setState(SquareState newstate) {
+		state = newstate;
+		return state;
 	}
 }
