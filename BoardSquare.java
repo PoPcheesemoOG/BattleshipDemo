48e6fdc6cc6156ca53ee01d8afafeea431f133cb
Paul Helske <116106386+PoPcheesemoOG@users.noreply.github.com>
2023-07-10 16:55:33 -0600

    Update BoardSquare.java

--- BoardSquare.java
+++ BoardSquare.java
@@ -1,3 +1,6 @@
+/* Name: Paul Helske
+ * Date: 06/20/23
+ */
 public class BoardSquare {
 
 	public enum SquareState {Water, Hidden_Boat_Piece, Exposed_Boat_Piece};
@@ -5,16 +8,14 @@ public class BoardSquare {
 
 	static int height = GameBoard.height;
 	static int width = GameBoard.width;
-	
+
 	public int boatRow;
 	public int boatCol;
-	
+
 	public BoardSquare(SquareState newstate, int boatRow, int boatCol) {
 		state = newstate;
-		// boatRow = Battleship.rowsize;
-		// boatCol = Battleship.colsize;
 	}
-	
+
 	public SquareState setState(SquareState newstate) {
 		state = newstate;
 		return state;
